1945e62de4f0aadddc057906a76572118d4962fd
Heitor Bartolomei <heitorbartolomei26@gmail.com>
2026-07-30 11:33:19 -0300

    Primeiro commit do projeto

---
+++ README.md
@@ -0,0 +1,20 @@
+## Dependências
+
+- Python 3.10+
+- yt-dlp
+
+Instalação:
+
+pip install -r requirements.txt
+
+## FFmpeg
+
+Baixe o FFmpeg em:
+
+https://www.gyan.dev/ffmpeg/builds/
+
+Extraia para:
+
+C:\ffmpeg-8.1.2-essentials_build
+
+Ou adicione a pasta `bin` ao PATH do Windows.
---
+++ yt_downloader.py
@@ -0,0 +1,36 @@
+# Este programa baixa músicas do YouTube utilizando yt-dlp e FFmpeg.
+
+# pip install yt-dlp OU python -m pip install yt-dlp
+# Link do FFmpeg: https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
+#(baixe e extraia o .zip)
+
+from yt_dlp import YoutubeDL
+
+print("Cole as URLs dos videos ou playlists (uma por linha)")
+print("Quando terminar, pressione Enter em uma linha vazia!.\n")
+
+urls = []
+
+while True:
+    url = input()
+
+    if url == "":
+        break
+
+    urls.append(url)
+
+opcoes = {
+    'format': 'bestaudio/best',
+    'outtmpl': r'D:\%(title)s.%(ext)s',                             # Escolha o caminho de destino para salvar os arquivos baixados
+    'ffmpeg_location': r'C:\ffmpeg-8.1.2-essentials_build\bin',     # Escolha o caminho onde o FFmpeg está instalado
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+
+with YoutubeDL(opcoes) as ydl:
+    ydl.download(urls)
+
+print("\nDownload concluído!\n")
